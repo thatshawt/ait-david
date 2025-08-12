c5bf408e96c0cbbc3745a823ccde92b3a5cc5b7f
looking for ai idk bruv <david-ai@vps-7e402fe9.vps.ovh.us>
2025-08-12 08:15:50 +0000

    added more turing machine stuff

--- c-turing/main.c
+++ c-turing/main.c
@@ -2,114 +2,187 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define STATES 6
+#define T_STATES 6
 
-#define TRUE 1
-#define FALSE 0
-#define TAPE_SIZE 13000
+#define T_TRUE 1
+#define T_FALSE 0
+#define T_TAPE_SIZE 13000
 
-#define RIGHT 0
-#define LEFT 1
+#define T_RIGHT 0
+#define T_LEFT 1
 
-#define m_SET_TRANSITION(table, state, symbol, next_state, write, action) table[state][symbol] = (struct turing_transition) {next_state,write,action}
+#define m_SET_TRANSITION(table, state, symbol, next_state, write, action) table[state][symbol] = (struct t_transition) {next_state,write,action}
 
-struct turing_transition{
+struct t_transition{
 	int next_state;
 	int write;
 	int action;
 };
 
-int state = 0;
-int tape[TAPE_SIZE] = {0};
-int tape_i = 0;
+int t_state = 0;
+int t_tape[T_TAPE_SIZE] = {0};
+int t_tape_i = 0;
 
-struct turing_transition (*transitions)[STATES][2];
+struct t_transition (*t_transitions)[T_STATES][2];
 
-int turing_reset(){
-	state = 0;
-	//tape = {0};
-	//memset(tape, 0, TAPE_SIZE * sizeof tape);
-	tape_i = 0;
-	// TODO reset transitions
-}
+//#define T_DEBUG
 
-//#define TURING_DEBUG 0
-
-int turing_step(){
+int t_step(){
 	// halting state
-	if(state == 0){
-		return TRUE;
+	if(t_state == 0){
+		return T_TRUE;
 	}
 
-	#ifdef TURING_DEBUG
-	printf("tapei: %d\n", tape_i);
+	#ifdef T_DEBUG
+	printf("t_tapei: %d\n", t_tape_i);
 	#endif
-	int fixed_tape_i = (tape_i+TAPE_SIZE) % TAPE_SIZE;
-	if(fixed_tape_i < 0 || fixed_tape_i >= TAPE_SIZE){
+	int fixed_tape_i = (t_tape_i+T_TAPE_SIZE) % T_TAPE_SIZE;
+	if(fixed_tape_i < 0 || fixed_tape_i >= T_TAPE_SIZE){
 		printf("%d tapei too small or too large\n", fixed_tape_i);
-		return TRUE;
+		return T_TRUE;
 	}
-	int tape_symbol = tape[fixed_tape_i];
-	#ifdef TURING_DEBUG
+	int tape_symbol = t_tape[fixed_tape_i];
+	#ifdef T_DEBUG
 		printf("fixed_tape_i: %d, state: %d, symbol: %d\n",
-			fixed_tape_i, state, tape_symbol);
+			fixed_tape_i, t_state, tape_symbol);
 	#endif
 
-	struct turing_transition t_entry = (*transitions)[state][tape_symbol];
-	#ifdef TURING_DEBUG
+	struct t_transition t_entry = (*t_transitions)[t_state][tape_symbol];
+	#ifdef T_DEBUG
 		printf("next_state: %d, write: %d, action: %d\n",
 			t_entry.next_state, t_entry.write, t_entry.action);
 	#endif
 
-	tape[fixed_tape_i] = t_entry.write;
-	state = t_entry.next_state;
-	if(t_entry.action == RIGHT){
-		tape_i++;
-	}else if(t_entry.action == LEFT){
-		tape_i--;
+	t_tape[fixed_tape_i] = t_entry.write;
+	t_state = t_entry.next_state;
+	if(t_entry.action == T_RIGHT){
+		t_tape_i++;
+	}else if(t_entry.action == T_LEFT){
+		t_tape_i--;
 	}
 
-	#ifdef TURING_DEBUG
+	#ifdef T_DEBUG
 	printf("hi\n\n");
 	#endif
 
-	if(state == 0){
-		return TRUE;
+	if(t_state == 0){
+		return T_TRUE;
 	}else{
-		return FALSE;
+		return T_FALSE;
 	}
 }
 
-struct turing_transition bb5_transition_table[STATES][2];
+struct t_transition bb5_transition_table[T_STATES][2];
 
-void init_bb5(){
-        m_SET_TRANSITION(bb5_transition_table, 1,0, 2,1,RIGHT);
-        m_SET_TRANSITION(bb5_transition_table, 1,1, 3,1,LEFT);
+//typedef struct t_transition_string { int x[5]; }
+//	t_transition_string;
+//typedef struct t_transition_func_string { t_transition_string x[T_STATES*2]; }
+//	t_transition_func_string;
+typedef struct t_transition t_trans_func_table[T_STATES][2];
+#define T_TRANS_MAX_FUNC_STR_SIZE (T_STATES-1)*2*5 + 1
+typedef int t_trans_func_str[T_TRANS_MAX_FUNC_STR_SIZE];
+// 5,1, 1,0,0
+// (state,symbol, next_state,write,action)+
+// 
 
-	m_SET_TRANSITION(bb5_transition_table, 2,0, 3,1,RIGHT);
-	m_SET_TRANSITION(bb5_transition_table, 2,1, 2,1,RIGHT);
+enum t_trans_func_str_options{
+	T_STR_OPT_EXPLICIT_STATE_SYMBOL,
+	T_STR_OPT_IMPLICIT_STATE_SYMBOL
+};
 
-	m_SET_TRANSITION(bb5_transition_table, 3,0, 4,1,RIGHT);
-	m_SET_TRANSITION(bb5_transition_table, 3,1, 5,0,LEFT);
+void t_load_trans_func_str(
+		t_trans_func_table func_table,
+		t_trans_func_str func_str){
+	if(func_str[0] == T_STR_OPT_EXPLICIT_STATE_SYMBOL){
+	for(int i=1;i<T_TRANS_MAX_FUNC_STR_SIZE;i+=5){
+		int state = func_str[i];
+		int symbol = func_str[i+1];
+		int next_state = func_str[i+2];
+		int write = func_str[i+3];
+		int action = func_str[i+4];
+		m_SET_TRANSITION(func_table, state,symbol, next_state,write,action);
+	}
+	}else if(func_str[0] == T_STR_OPT_IMPLICIT_STATE_SYMBOL){
+	int state = 0;
+	int symbol = 0;
+	for(int i=1;i<T_TRANS_MAX_FUNC_STR_SIZE;i+=3){
+		if(((i-1)/3)%2 == 0)state++;
+		if(symbol == 2)symbol = 0;
+                int next_state = func_str[i+0];
+                int write = func_str[i+1];
+                int action = func_str[i+2];
+                m_SET_TRANSITION(func_table, state,symbol++, next_state,write,action);
+        }
+	}
+}
 
-	m_SET_TRANSITION(bb5_transition_table, 4,0, 1,1,LEFT);
-	m_SET_TRANSITION(bb5_transition_table, 4,1, 4,1,LEFT);
 
-	m_SET_TRANSITION(bb5_transition_table, 5,0, 0,1,RIGHT);
-	m_SET_TRANSITION(bb5_transition_table, 5,1, 1,0,LEFT);
 
-	transitions = &bb5_transition_table;
+void init_bb5_new(){
+	t_trans_func_str bb5_func_str1 = {
+        T_STR_OPT_EXPLICIT_STATE_SYMBOL,
+        1,1, 3,1,T_LEFT,
+        1,0, 2,1,T_RIGHT,
+        2,0, 3,1,T_RIGHT,
+        2,1, 2,1,T_RIGHT,
+        3,0, 4,1,T_RIGHT,
+        3,1, 5,0,T_LEFT,
+        4,0, 1,1,T_LEFT,
+        4,1, 4,1,T_LEFT,
+        5,0, 0,1,T_RIGHT,
+        5,1, 1,0,T_LEFT
+	};
 
-	state = 1;
+	t_trans_func_str bb5_func_str2 = {
+	T_STR_OPT_IMPLICIT_STATE_SYMBOL,
+        2,1,T_RIGHT,
+        3,1,T_LEFT,
+        3,1,T_RIGHT,
+        2,1,T_RIGHT,
+        4,1,T_RIGHT,
+        5,0,T_LEFT,
+        1,1,T_LEFT,
+        4,1,T_LEFT,
+        0,1,T_RIGHT,
+        1,0,T_LEFT
+        };
+
+	t_load_trans_func_str(bb5_transition_table, bb5_func_str2);
+	t_transitions = &bb5_transition_table;
+	t_state = 1;
+}
+
+int triple_maxes[3] = {T_STATES-1,1,1};
+int triple[3] = {0,0,0};
+int next_triple(){
+	if(++triple[2] > triple_maxes[2]){
+		triple[2] = 0;
+		if(++triple[1] > triple_maxes[1]){
+			triple[1] = 0;
+			if(++triple[0] > triple_maxes[0]){
+				triple[0] = triple_maxes[0];
+				triple[1] = triple_maxes[1];
+				triple[2] = triple_maxes[2];
+				return T_TRUE;
+			}
+		}
+	}
+	return T_FALSE;
+}
+void reset_triple(){
+	triple[0] = 0;
+	triple[1] = 0;
+	triple[2] = 0;
 }
 
 int main(){
 	printf("BB5\n");
 
-	init_bb5();
+	//init_bb5();
+	init_bb5_new();
 
 	int counter=1;
-	while(turing_step() != TRUE){
+	while(t_step() != T_TRUE){
 		counter++;
 		//keep doing step until HALT
 	}
